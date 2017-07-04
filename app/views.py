8ce42d23bc7c4cc8fa43002cd3142dd0956f544b
kakutani <pekepooooong@gmail.com>
2017-07-04 19:12:48 +0900

    viewにモジュール追加とか

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from requests_oauthlib import OAuth1Session
+import json
+import re
+import os
+import requests
+import sys, codecs
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
+
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 def index(request):
     return render(request,
         'index.html',
     )
+
+def tweet(tweet):
+    pass
