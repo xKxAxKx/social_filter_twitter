f1b32312377b55a5343482e8882a33f020cc080d
kakutani <pekepooooong@gmail.com>
2017-07-04 18:35:34 +0900

    [update]index.html作ったりとか

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-### Djangoの起動
+### Install package
+```
+$ pip install -r requirement.txt
+```
+
+### Run Django(Local)
 ```
 $ python manage.py runserver --settings=social_filter_twitter.settings_local
 ```
--- social_filter_twitter/settings_local.py
+++ social_filter_twitter/settings_local.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR, "app/static/"),
+)
+
+# static_root、サーバで動かす場合に使う
+# STATIC_ROOT = "/home/jacob/projects/mysite.com/sitestatic"
