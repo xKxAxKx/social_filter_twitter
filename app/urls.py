1d655bc2b705ace623c187bb89c4a14d69577d6b
xKxAxKx <pekepooooong@gmail.com>
2017-07-17 21:05:56 +0900

    update

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,5 @@ from app import views
 urlpatterns = [
     url(r'^home/', views.home.as_view(), name='home'),
     url(r'^logout/', views.logout, name='logout'),
-    url(r'^', views.index.as_view(), name='index'),
+    url(r'^$', views.index.as_view(), name='index'),
 ]
--- social_filter_twitter/urls.py
+++ social_filter_twitter/urls.py
@@ -7,5 +7,5 @@ import app.urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('social_django.urls', namespace = 'social')),
-    url(r'^$', include(app.urls)),
+    url(r'^', include(app.urls)),
 ]
