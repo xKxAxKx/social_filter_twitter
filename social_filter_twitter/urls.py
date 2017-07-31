1228a7f73ef4b33481a9e63512433cbb0ac58ea4
kakutani <kakutani@aka.jp>
2017-07-31 18:55:09 +0900

    update

--- social_filter_twitter/urls.py
+++ social_filter_twitter/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin, auth
 import app.urls
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'', include('social_django.urls', namespace = 'social')),
     url(r'', include(app.urls)),
 ]
