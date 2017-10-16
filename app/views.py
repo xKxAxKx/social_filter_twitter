348f06051c0bc1fc8751682bf441d526e8a3b436
kakutani <kakutani@aka.jp>
2017-10-16 12:04:21 +0900

    [update]PEP8

--- app/views.py
+++ app/views.py
@@ -4,7 +4,9 @@ import json
 import re
 import os
 import requests
-import sys, codecs
+import sys
+import codecs
+import pandas
 # sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
 from social_django.models import UserSocialAuth
 import twitter
@@ -17,18 +19,18 @@ from django.conf import settings
 from django import forms
 from .forms import TweetForm
 
-# 形態素解析関連
+#形態素解析関連
 import MeCab
 mecab = MeCab.Tagger('-Ochasen')
-import pandas
 
-PN_TXT= 'pn_ja.dic.txt'
 
-pn_df = pandas.read_csv(PN_TXT,\
-                    sep=':',
-                    encoding='utf-8',
-                    names=('Word','Reading','POS', 'PN')
-                   )
+PN_TXT = 'pn_ja.dic.txt'
+
+pn_df = pandas.read_csv(PN_TXT,
+                        sep=':',
+                        encoding='utf-8',
+                        names=('Word', 'Reading', 'POS', 'PN')
+                        )
 
 # PN Tableをデータフレームからdict型に変換しておく
 word_list = list(pn_df['Word'])
@@ -38,8 +40,8 @@ pn_dict = dict(zip(word_list, pn_list))
 
 def index(request):
     return render(request,
-        'index.html',
-    )
+                  'index.html',
+                  )
 
 
 @login_required
@@ -55,7 +57,10 @@ def home(request):
         if form.is_valid():
             tweet = request.POST['tweet']
 
-            tweet = tweet_post(tweet, user_oauth_token, user_oauth_token_sercret)
+            tweet = tweet_post(tweet,
+                               user_oauth_token,
+                               user_oauth_token_sercret
+                               )
 
             if tweet:
                 message = '「{}」とツイートしました'.format(tweet)
@@ -64,28 +69,31 @@ def home(request):
 
             form = TweetForm
             return render(request,
-                'home.html',
-                dict(account_name = account_name, form = form, message = message)
-            )
+                          'home.html',
+                          dict(account_name=account_name,
+                               form=form,
+                               message=message
+                               )
+                          )
     else:
         if request.user.is_authenticated():
             return render(request,
-                'home.html',
-                dict(account_name = account_name, form = form)
-            )
+                          'home.html',
+                          dict(account_name=account_name, form=form)
+                          )
         else:
             return redirect('index')
 
 
-# ツイートを形態素解析で分割する
+#ツイートを形態素解析で分割する
 def tweet_mecab_analysis(tweet):
     divided_tweet = mecab.parse(tweet)
-    divided_tweet_split = divided_tweet.split('\n') # 形態素解析した文書を配列にする
-    divided_tweet_lines = divided_tweet_split[0:-2] # divided_tweet_splitから'EOS', ''を除去
+    divided_tweet_split = divided_tweet.split('\n')  #形態素解析した文書を配列にする
+    divided_tweet_lines = divided_tweet_split[0:-2]  # divided_tweet_splitから'EOS', ''を除去
     diclist = []
     for word in divided_tweet_lines:
-        l = re.split('\t|,',word)
-        d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'POS3':l[3]}
+        l = re.split('\t|,', word)
+        d = {'Surface': l[0], 'POS1': l[1], 'POS2': l[2], 'POS3': l[3]}
         diclist.append(d)
 
     return(diclist)
@@ -95,7 +103,7 @@ def tweet_mecab_analysis(tweet):
 def add_pnvalue(diclist_old):
     diclist_new = []
     for word in diclist_old:
-        base = word['POS2'] # 個々の辞書から基本形を取得
+        base = word['POS2']  #個々の辞書から基本形を取得
         if base in pn_dict:
             pn = float(pn_dict[base])
         else:
@@ -105,7 +113,7 @@ def add_pnvalue(diclist_old):
     return(diclist_new)
 
 
-# ツイートのネガポジ値を判定する関数
+#ツイートのネガポジ値を判定する関数
 def get_tweet_score(diclist):
     pn_list = []
     for word in diclist:
@@ -115,22 +123,22 @@ def get_tweet_score(diclist):
     if len(pn_list) > 0:        # 「全部notfound」じゃなければ
         score = mean(pn_list)
     else:
-        score = 0 # 全部notfoundならゼロにする
+        score = 0  # 全部notfoundならゼロにする
     return(score)
 
 
-# 平均値を出す関数
+#平均値を出す関数
 def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
 
 
 # twitterにポストする関数
 def tweet_post(tweet, user_oauth_token, user_oauth_token_sercret):
-    auth = twitter.OAuth(consumer_key = settings.SOCIAL_AUTH_TWITTER_KEY,
-                     consumer_secret = settings.SOCIAL_AUTH_TWITTER_SECRET,
-                     token = user_oauth_token,
-                     token_secret = user_oauth_token_sercret)
-    t = twitter.Twitter(auth = auth)
+    auth = twitter.OAuth(consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
+                         consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
+                         token=user_oauth_token,
+                         token_secret=user_oauth_token_sercret)
+    t = twitter.Twitter(auth=auth)
 
     # ツイートのネガポジ判定&ツイートの差し替え
     # スコア値は微調整が必要だな...
@@ -140,11 +148,11 @@ def tweet_post(tweet, user_oauth_token, user_oauth_token_sercret):
     if tweet_score <= -0.50:
         tweet = "にゃーん"
 
-    # 実際にツイートしてみる
+    #実際にツイートしてみる
     # 例外が発生した場合、tweet=にゃーんだったらにゃーんを追加して再チャレンジ
     # にゃーん以外だったらNoneで返す(エラーの内容は問わず)
     try:
-        t.statuses.update(status = tweet)
+        t.statuses.update(status=tweet)
         return tweet
     except:
         if tweet.startswith("にゃーん"):
@@ -155,7 +163,7 @@ def tweet_post(tweet, user_oauth_token, user_oauth_token_sercret):
                 else:
                     tweet += "にゃーん"
                 try:
-                    t.statuses.update(status = tweet)
+                    t.statuses.update(status=tweet)
                     return tweet
                 except:
                     continue
